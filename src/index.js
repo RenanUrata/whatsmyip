acdf485c64124b1cca28e3df2fba1b4e51cb4758
Renan Urata <renan.urata@gmail.com>
2020-09-11 22:21:08 -0300

    mobile fixed up, clipboard functinality addressed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 // import GMaps from './gmaps';
 const init = () => {
+    
+const copySvg = document.querySelector('.copySvg')
+copySvg.addEventListener('click', ()=>{
+    const ipIU = document.querySelector('.ip')
+    ipIU.select();
+    ipIU.setSelectionRange(0, 99999);
+    
+    try {
+        document.execCommand("copy") ?
+        alert('Ip is now on clipBoard for your convinience.') : 
+        alert('Something went Wrong... Try again') 
+    } catch (error) {
+        console.error('Unable to copy', error);
+    }
 
+}, false)
 const newMap = (lat, lng ) => new GMaps({
     div: '#map',
     lat,
@@ -13,13 +28,14 @@ const query = fetch('https://ipinfo.io/json').then(response => response.json())
     {let {ip, org, city, region, country, loc, postal} = data
     newMap(loc.substring(0,8), loc.substring(9,loc.length) )
     const ipUI = document.querySelector('.ip')
-    ipUI.innerHTML = ip
+    ipUI.value = ip
     const locationUI = document.querySelector('.location')
     locationUI.innerHTML = `${city}, ${region}, ${country}`
 
 })
 .catch(err => console.error(err));
  
+
 
 }
 
