ab6aa7f1a7a821ad74723bd6a558e4979d77a648
Renan Urata <renan.urata@gmail.com>
2020-09-12 05:59:11 -0300

    embeded map implemented

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // import GMaps from './gmaps';
 const init = () => {
   const copySvg = document.querySelector(".copySvg");
+  const mapDiv = document.querySelector("#map");
+  let location =  { lat: 0,lng: 0}
   copySvg.addEventListener(
     "click",
     () => {
@@ -18,24 +20,24 @@ const init = () => {
     },
     false
   );
-  const newMap = (lat, lng) =>
-    new GMaps({
-      div: "#map",
-      lat,
-      lng,
-    });
 
-  const query = fetch("https://ipinfo.io/json")
+
+  fetch("https://ipinfo.io/json")
     .then((response) => response.json())
     .then((data) => {
       let { ip, org, city, region, country, loc, postal } = data;
-      newMap(loc.substring(0, 8), loc.substring(9, loc.length));
+      const lat = parseFloat(loc.substring(0, 8))
+      const lng = parseFloat(loc.substring(9, loc.length))
+      // location = { lat, lng}
+      // localStorage.setItem('location', JSON.stringify(location))
       const ipUI = document.querySelector(".ip");
       ipUI.value = ip;
       const locationUI = document.querySelector(".location");
       locationUI.innerHTML = `${city}, ${region}, ${country}`;
     })
     .catch((err) => console.error(err));
+
+
 };
 
 init();
